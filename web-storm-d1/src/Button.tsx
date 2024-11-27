6a36649821c268632a268c7ff299e2037f11757b
dreaca <buddhikadreaca@gmail.com>
2024-11-27 10:25:03 +0530

    creating components and stuff

---
+++ web-storm-d1/src/Button.tsx
@@ -0,0 +1,15 @@
+export default function Button() {
+    return (
+        <button>Click Me!</button>
+    )
+}
+export function FancyButton() {
+    return (
+        <button>Fancy Click Me!</button>
+    )
+}
+export function SmallButton() {
+    return (
+        <button>Small Click Me!</button>
+    )
+}
