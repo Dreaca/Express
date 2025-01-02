8e540ad67ef878272e13cc5361c34faad99f632c
dreaca <buddhikadreaca@gmail.com>
2025-01-02 10:42:02 +0530

    react redux

--- react-redux-practice/src/App.tsx
+++ react-redux-practice/src/App.tsx
@@ -3,14 +3,18 @@ import './App.css'
 import {useDispatch, useSelector} from "react-redux";
 
 function App() {
-    const count = useSelector(state=>state)
+    const count = useSelector(state=>state.count)
     const dispatch = useDispatch();
+    const visible = useSelector(state=>state.visibility)
   return (
     <>
-        {count}
+        <div>
+            {visible && count}
+        </div>
         <br/>
       <button onClick={()=>dispatch({type:"ADD_COUNTER"})}>Increment</button>
       <button onClick={()=>dispatch({type:"REMOVE_COUNT"})}>Decrement</button>
+        <button onClick={()=>dispatch({type:"HIDE"})}>Toggle</button>
     </>
   )
 }
--- react-redux-practice/src/reducers/CountReducer.ts
+++ react-redux-practice/src/reducers/CountReducer.ts
@@ -1,11 +1,16 @@
-const initialState = 0
+const initialState = {
+    count: 0,
+    visibility:true
+}
 
-export function CountReducer(state = initialState, action : {type: string, payload : number}) {
+export function CountReducer(state = initialState, action : {type: string, payload : string}) {
     switch (action.type) {
         case 'ADD_COUNTER':
-            return state + 1
+            return {...state, count: state.count + 1 }
         case 'REMOVE_COUNT':
-            return state - 1
+            return {...state, count: state.count - 1 }
+        case 'HIDE':
+            return {...state, visibility: !state.visibility}
         default:
             return state
     }
