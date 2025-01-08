04ac07be7df2f65bddfd381b6f802104e29d609a
dreaca <buddhikadreaca@gmail.com>
2025-01-08 10:49:43 +0530

    filesystem

--- Node/Demo.js
+++ Node/Demo.js
@@ -1,2 +1,8 @@
-"use strict";
-console.log("Hello world!");
+import http from 'http';
+import fs from 'fs';
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    fs.writeFileSync('demo.txt', (req.url + ':' + req.method));
+    process.exit();
+});
+server.listen(3000);
