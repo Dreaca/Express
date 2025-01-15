b31abd7164434e01d0d41eee9af497f806a30151
dreaca <buddhikadreaca@gmail.com>
2025-01-15 11:43:49 +0530

    fallback function added

--- server.ts
+++ server.ts
@@ -10,3 +10,7 @@ app.use('/customer',customerRoutes)
 app.listen(3000, (err)=>{
     console.log("Server started on port 3000");
 });
+
+app.use('/',(req,res,next)=>{
+    res.status(401).send('Not Found');
+})
