461bc46ce85cdfcbe269449c18266f59369820cb
dreaca <buddhikadreaca@gmail.com>
2025-01-15 09:47:41 +0530

    init commit

---
+++ server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.use('/', (req, res) => {
+    res.send('Hello World!');
+})
+app.listen(3000, (err)=>{
+    console.log("Server started on port 3000");
+});
