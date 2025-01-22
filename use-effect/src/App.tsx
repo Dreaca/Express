0b8eea8da07e3eaa636647b8c641855d4bd02678
dreaca <buddhikadreaca@gmail.com>
2025-01-22 10:22:15 +0530

    axios added

--- use-effect/src/App.tsx
+++ use-effect/src/App.tsx
@@ -1,12 +1,40 @@
 import {useEffect, useState} from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
     const [count2, setCount2] = useState(0)
 
+  const api= axios.create({
+    baseURL: 'http://localhost:3000',
+  })
+
+    // async function getAPI(){
+    //   const response = await fetch('http://localhost:3000/get')
+    //     const data = await response.text()
+    //     console.log(data)
+    // }
+    async function getAPI(){
+      const response = await api.get('/get')
+      console.log(response.data)
+    }
+    async function postAPI(){
+
+      const customer = {
+        'name':"Buddhika",
+        'email':"buddhika@gmail.com",
+        'phone':"1234567890"
+      };
+
+      const response = await api.post('/post',customer);
+
+      console.log(response.data)
+    }
+
     useEffect(() => {
-        console.log("render")
+        getAPI();
+        postAPI();
     },[count2])
   return (
     <>
